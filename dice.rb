9475816a50d47143eae17e4c6a630486eedf3108
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-07-20 19:55:42 +0000

    completed dice and added routes to root

--- dice.rb
+++ dice.rb
@@ -1,16 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do 
-  "Hello World"
-end
-
-get("/zebra") do
-  "We must add a route for each path we want to support"
-end
-
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
+  "<h1>Dice Roll</h1>
+  <ul>
+    <li><a href=/dice/2/6>Roll two 6-sided dice</a></li>
+    <li><a href=/dice/2/10>Roll two 10-sided dice</a></li>
+    <li><a href=/dice/1/20>Roll one 20-sided dice</a></li>
+    <li><a href=/dice/5/4>Roll five 4-sided dice</a></li>
+  </ul>"
 end
 
 get("/dice/2/6") do
@@ -40,5 +43,19 @@ get("/dice/1/20") do
   outcome = "You rolled a #{dice}."
 
   "<h1>1d20</h1
+  <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+
+  outcome = "You rolled #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+
+  "<h1>5d4</h1>
   <p>#{outcome}</p>"
 end
